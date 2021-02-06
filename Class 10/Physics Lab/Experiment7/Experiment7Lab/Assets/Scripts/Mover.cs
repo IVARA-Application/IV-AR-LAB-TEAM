f63220c9bc6dcbe1d6c6e1180f3fc73789978b29
Jatin Agrawal <jatinagrawal2000@gmail.com>
2021-02-06 11:02:28 +0530

    Adding Experiment7

---
+++ Class 10/Physics Lab/Experiment7/Experiment7Lab/Assets/Scripts/Mover.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    #region Variables
+    public float InitialPosValue = 0f;
+    public int multiplier = 0;
+    public DistanceType unit;
+    public bool moveInOppositeDirection =false;
+
+    [SerializeField]
+    private UnityEngine.UI.Text textObj;
+    [SerializeField]
+    public UnityEngine.UI.Slider slider;
+
+    private Vector3 initialPos;
+    private Transform objTransform;
+    private float calculatedPosValue = 0f;
+
+    #endregion
+
+    #region Properties
+    private float LensDislocation
+    {
+        get { return slider.value; }
+    }
+    public float CurrentEquipmentPosition
+    {
+        get { return calculatedPosValue; }
+    }
+
+    #endregion
+
+    private void Awake()
+    {
+        objTransform = gameObject.GetComponent<Transform>();
+        initialPos = transform.localPosition;
+    }
+
+    public void OnSliderChange()
+    {
+        objTransform.localPosition = new Vector3(initialPos.x + ((moveInOppositeDirection)? LensDislocation: -LensDislocation), initialPos.y, initialPos.z);
+        if(textObj != null)
+        {
+            calculatedPosValue = InitialPosValue - multiplier * slider.value;
+            textObj.text = calculatedPosValue.ToString() + unit.ToString();
+        }
+    }
+
+}
